dd41b30459f2323779f395cb165f5aff5c2a0ebe
Chris Wilcox <ckwilcox@gmail.com>
2022-11-10 23:12:45 -1000

    cleanup docker

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-pipenv requirements > requirements.txt &&
-    docker build --tag "$DOCKER_IMAGE_TAG" . &&
+docker build --tag "$DOCKER_IMAGE_TAG" . &&
     echo $CR_PAT | docker login ghcr.io -u topher515 --password-stdin &&
     docker push "$DOCKER_IMAGE_TAG" &&
     printf "Built and pushed '$DOCKER_IMAGE_TAG'"
